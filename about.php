a0cbe9395bc7184414044470d12e87bba7d90ba3
Dom <domwheeler4@gmail.com>
2022-02-21 21:55:47 +0000

    added to about page

--- about.php
+++ about.php
@@ -53,14 +53,34 @@
     </div>
 </div>
 
-    <div class="space">
+    <!-- <div class="space">
         <br>
+    </div> -->
+
+    <div class="headteacher-welcome-container">
+        
+            <div class="welcome-text">
+                <h1 class="center-text">Vision Statement</h1>
+                <p class="center-text">"We believe that education should take place in a fully inclusive environment with equal opportunities for all and that all children should learn to value religious and cultural differences. Our school strives to be at the centre of the local community with positive and effective links to the wider and global communities."</p>
+            </div>
+        
     </div>
 
     <div class="headteacher-welcome-container">
-        <div class="head-welcome">
-            <div class="welcome-text">
-                <p>This will be some text explaining about the school.</p>
+        <div class="head-welcome-block">
+            <h1 class="center-text">Meet the Staff</h1>
+            <div class="meet-row-left">
+                <img src="images/headteacher.png" class="staff-photo" height="125" width="125">
+                <span> This is the Headteacher of Maple Tree Primary, Ms. Veronica Pickles. Her favourite part of the school is the sense of community.</span>
+            </div>
+            <br>
+            <div class="meet-row-right">
+                <span> Poppy is the therapy dog for the Infants (Years 1 to 3) and she is 11 years old. She likes sausages, ice cream, and sleeping!</span>
+                <img src="images/dog1.png" class="staff-photo" height="125" width="125">
+            </div>
+            <div class="meet-row-left">
+                <img src="images/dog2.png" class="staff-photo" height="125" width="125">
+                <span> Daisy is the therapy dog for the Juniors (Years 4 to 6) and she is one year old. She likes chasing birds, long walks, and eating carrots!</span>
             </div>
         </div>
     </div>
