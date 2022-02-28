936c4b6972f32ffdc7673ea0c74b8ede258279b6
modjw <domwheeler4@gmail.com>
2022-02-28 18:52:56 +0000

    code comments

--- authenticate.php
+++ authenticate.php
@@ -1,25 +1,24 @@
 <?php
 session_start();
-// Change this to your connection info.
+// Connection info
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
 $DATABASE_NAME = 'school';
 
-// Try and connect using the info above.
+// Establish connection
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if ( mysqli_connect_errno() ) {
 	// If there is an error with the connection, stop the script and display the error.
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 
-// Now we check if the data from the login form was submitted, isset() will check if the data exists.
+// Check that data has been set in the fields
 if ( !isset($_POST['username'], $_POST['password']) ) {
-	// Could not get the data that should have been sent.
 	exit('Please fill both the username and password fields!');
 }
 
-// Prepare our SQL, preparing the SQL statement will prevent SQL injection.
+// Preparing SQL to avoid SQL injection
 if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
 	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
 	$stmt->bind_param('s', $_POST['username']);
@@ -33,8 +32,7 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?'
         // Account exists, now we verify the password.
         // Note: remember to use password_hash in your registration file to store the hashed passwords.
         if (password_verify($_POST['password'], $password)) {
-            // Verification success! User has logged-in!
-            // Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
+            // Successful Login
             session_regenerate_id();
             $_SESSION['loggedin'] = TRUE;
             $_SESSION['name'] = $_POST['username'];
